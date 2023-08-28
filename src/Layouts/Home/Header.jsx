2a2336a404efe1190099c0c74ebfabe5c8a61d11
jawad Bin Azam <abidibnazam@gmail.com>
2023-08-28 09:40:33 +0600

    saving progress

---
+++ src/Layouts/Home/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-[#fffafa] border border-red-700">
+      <h1 className="text-7xl font-semibold">
+        Welcome to the <br />
+        community forum for developers
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,12 @@
+import Header from "@/Layouts/Home/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello</div>
+    <main className="flex min-h-screen flex-col justify-between p-10">
+      <div>
+        <Header></Header>
+      </div>
     </main>
   );
 }
